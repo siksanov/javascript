0ec748ff5923165999607a27c5574af86898a71a
Sergey Iksanov <sergey@iksanov.iksanov.org>
2025-11-16 00:41:57 +0500

    stage 9 completed

---
+++ books/vdronov/9/s9.4/2/8.4.js
@@ -0,0 +1,30 @@
+function createSpoiler(element, state=false, decoration='') {
+    let spoilerState = false;
+
+    function onHeaderClick() {
+        spoilerState = !spoilerState;
+        if (spoilerState)
+            this.parentElement.classList.add('opened');
+        else
+            this.parentElement.classList.remove('opened');
+    }
+
+    const header = element.querySelector('h6');
+    element.classList.add('spoiler');
+    spoilerState = state;
+    if (spoilerState)
+        element.classList.add('opened');
+    if (decoration)
+        element.classList.add(decoration);
+    header.addEventListener('click', onHeaderClick);
+}
+
+function createSpoilers(...elements) {
+    if (Array.isArray(elements[0]))
+        elements = elements.flat()
+    elements.forEach((v, k) => {
+        if (typeof v === 'string')
+            v = document.getElementById(v);
+        createSpoiler(v,...k);
+    })
+}
